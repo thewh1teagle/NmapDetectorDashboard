26c00e3c54250707267b9f5f4b8832c2798a92a6
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2021-01-21 23:46:58 +0200

    scrolling

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,28 +1,36 @@
-$(document).ready(function () {
-    var trigger = $('.hamburger'),
-        overlay = $('.overlay'),
-       isClosed = false;
+var dashboard = $(".dashboard")
+var dashboard_ul = $(".list-group")
+// var ul_container = $(".ul-container")
+var ul_container = $(".ul-container")
+
+const add_li_element = function(IP, TIMESTAMP, PORTS) {
+  dashboard_ul.append(
+    `
+    <li class="list-group-item" style="padding-left: 50px; padding-right: 50px;">
+           <h5  style="display: inline; border-left: 1px solid red; border-right: 1px solid red; margin-left: 10px; margin-right: 10px;">IP ${IP}</h2>
+           <h5 style="display: inline; border-left: 1px solid red; border-right: 1px solid red;">TIMESTAMP ${TIMESTAMP}</h2>
+           <h5 style="display: inline; border-left: 1px solid red; border-right: 1px solid red;">PORTS ${PORTS}</h2>
+    </li>
+    `
+  )
+}
+
+console.log("hi")
+
+// dashboard_ul.scroll(function() {
+//   console.log("scroll")
+// })
+
+
+ul_container.on('scroll', function() {
   
-      trigger.click(function () {
-        hamburger_cross();      
-      });
-  
-      function hamburger_cross() {
-  
-        if (isClosed == true) {          
-          overlay.hide();
-          trigger.removeClass('is-open');
-          trigger.addClass('is-closed');
-          isClosed = false;
-        } else {   
-          overlay.show();
-          trigger.removeClass('is-closed');
-          trigger.addClass('is-open');
-          isClosed = true;
-        }
-    }
-    
-    $('[data-toggle="offcanvas"]').click(function () {
-          $('#wrapper').toggleClass('toggled');
-    });  
-  });
+  var ul_container_height = ul_container.height()
+  var current_position = ul_container.scrollTop()
+  // if (current_position >= ul_container_height / 100 * 75) {
+  //   console.log("loading..")
+  // }
+  console.log(current_position)
+  console.log(ul_container_height)
+})
+
+
